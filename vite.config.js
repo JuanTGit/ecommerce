51ac4b4f4deb3756f5640a87017c83b7a1ff08dc
Juan <j.tejeda46@yahoo.com>
2024-07-17 18:34:54 -0500

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': '/src/components',
-      '@views': '/src/views',
-    },
-  },
   build: {
-    outDir: 'build',
-  },
+    outDir: 'dist'
+  }
 });
